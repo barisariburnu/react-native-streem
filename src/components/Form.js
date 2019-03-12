4b7721722a70a8f53b4cc400de1f9488b00fe894
BURSA\baris.ariburnu <barisariburnu@gmail.com>
2019-03-12 14:06:58 +0300

    Add google places

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ImageBackground, Dimensions, Image } from 'react-native';
+import { 
+    View, Text, StyleSheet, ImageBackground, Dimensions, Image, TouchableOpacity 
+} from 'react-native';
+import RNGoolePlaces from 'react-native-google-places';
 import Button from '../commons/Button';
 import { strings } from '../lang/Strings';
 
@@ -7,13 +10,29 @@ const { width, height } = Dimensions.get('window');
 
 class Form extends Component {
 
-    renderSection(text) {
+    state = {
+        yourLocation: '',
+        itsLocation: '',
+        yourLngLat: [],
+        itsLngLat: [],
+        yourImgOk: require('../img/ok.png'),
+        itsImgOk: require('../img/ok.png')
+    }
+
+    componentWillMount() {
+        this.setState({
+            yourLocation: strings.yourLocation,
+            itsLocation: strings.itsLocation
+        });
+    }
+
+    renderSection(text, onPress, img) {
         return (
             <View style={styles.section}>
-                <View style={styles.subSection}>
+                <TouchableOpacity style={styles.subSection} onPress={() => onPress}>
                     <Text style={styles.textStyle}>{text}</Text>                    
-                    <Image source={require('../img/ok.png')} />
-                </View>
+                    <Image source={img} />
+                </TouchableOpacity>
             </View>
         );
     }
@@ -29,15 +48,43 @@ class Form extends Component {
         );
     }
 
+    openSearchModel(type) {
+        RNGoolePlaces.openAutocompleteModal()
+        .then((place) => {
+            if (type === 'your') {
+                this.setState({ 
+                    yourLocation: place.name,
+                    yourImgOk: require('../img/check.png'),
+                    yourLngLat: [place.latitude, place.longitude]
+                });
+            } else {
+                this.setState({ 
+                    itsLocation: place.name,
+                    itsImgOk: require('../img/check.png'),
+                    itsLngLat: [place.latitude, place.longitude]
+                 });
+            }
+        })
+        .catch(error => console.log(error.message));
+    }
+
     render() {
         return (
             <ImageBackground source={require('../img/bg.png')} style={styles.imageBackgroundStyle}>
 
                 <Image source={require('../img/logo.png')} />
 
-                { this.renderSection(strings.yourLocation) }
+                { this.renderSection(
+                    this.state.yourLocation, 
+                    () => this.openSearchModel('your'), 
+                    this.state.yourImgOk
+                )}
 
-                { this.renderSection(strings.itsLocation) }
+                { this.renderSection(
+                    this.state.itsLocation, 
+                    () => this.openSearchModel('its'), 
+                    this.state.itsImgOk
+                )}
 
                 <View style={styles.pickerMainViewStyle}>
                     { this.renderPickerButton(strings.yourPhoto) }
