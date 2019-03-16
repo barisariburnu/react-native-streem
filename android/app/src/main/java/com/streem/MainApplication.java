fba19a02cd05f9a97fd8ce2fb82b5e65b8072c39
barisariburnu <barisariburnu@gmail.com>
2019-03-16 12:44:42 +0300

    Bug Fix: Google Places

--- android/app/src/main/java/com/streem/MainApplication.java
+++ android/app/src/main/java/com/streem/MainApplication.java
@@ -3,6 +3,7 @@ package com.streem;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNGestureHandlerPackage(),
           new RNGooglePlacesPackage(),
           new ReactNativeLocalizationPackage()
       );
